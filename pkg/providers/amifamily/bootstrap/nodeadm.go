9f923df64896e36577d527ed07e3bfa2da70ee2d
Cameron McAvoy <cmcavoy@indeed.com>
2025-05-29 13:53:58 -0500

    Add env to set maximum-dead-containers cli flag for kubelet

--- pkg/providers/amifamily/bootstrap/nodeadm.go
+++ pkg/providers/amifamily/bootstrap/nodeadm.go
@@ -18,6 +18,7 @@ import (
 	"context"
 	"encoding/base64"
 	"fmt"
+	"os"
 	"strings"
 
 	admapi "github.com/awslabs/amazon-eks-ami/nodeadm/api"
@@ -94,6 +95,10 @@ func (n Nodeadm) getNodeConfigYAML() (string, error) {
 	config.Spec.Kubelet.Config = inlineConfig
 	if arg := n.nodeLabelArg(); arg != "" {
 		config.Spec.Kubelet.Flags = []string{arg}
+	}
+	// al2023: --maximum-dead-containers is deprecated with no replacement, so we can only set it via cli flags, not config
+	if maxDeadContainers := os.Getenv("INDEED_MAX_DEAD_CONTAINERS"); maxDeadContainers != "" {
+		config.Spec.Kubelet.Flags = append(config.Spec.Kubelet.Flags, fmt.Sprintf("--maximum-dead-containers=%s", maxDeadContainers))
 	}
 
 	// Convert to YAML at the end for improved legibility.
