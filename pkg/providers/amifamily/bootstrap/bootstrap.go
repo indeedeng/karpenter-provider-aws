74e757ec647f4686788270ea91631f8117f2204f
Cameron McAvoy <cmcavoy@indeed.com>
2025-05-29 13:53:58 -0500

    Add env to set maximum-dead-containers cli flag for kubelet

--- pkg/providers/amifamily/bootstrap/bootstrap.go
+++ pkg/providers/amifamily/bootstrap/bootstrap.go
@@ -17,6 +17,7 @@ package bootstrap
 import (
 	"context"
 	"fmt"
+	"os"
 	"sort"
 	"strings"
 
@@ -44,6 +45,11 @@ type Options struct {
 
 func (o Options) kubeletExtraArgs() (args []string) {
 	args = append(args, o.nodeLabelArg(), o.nodeTaintArg())
+
+	// al2: --maximum-dead-containers is deprecated with no replacement, so we can only set it via cli flags, not config
+	if maxDeadContainers := os.Getenv("INDEED_MAX_DEAD_CONTAINERS"); maxDeadContainers != "" {
+		args = append(args, fmt.Sprintf("--maximum-dead-containers=%s", maxDeadContainers))
+	}
 
 	if o.KubeletConfig == nil {
 		return lo.Compact(args)
@@ -73,6 +79,7 @@ func (o Options) kubeletExtraArgs() (args []string) {
 	if o.KubeletConfig.CPUCFSQuota != nil {
 		args = append(args, fmt.Sprintf("--cpu-cfs-quota=%t", lo.FromPtr(o.KubeletConfig.CPUCFSQuota)))
 	}
+
 	return lo.Compact(args)
 }
 
